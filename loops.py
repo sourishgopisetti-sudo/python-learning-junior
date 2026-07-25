dc0bb048e69c99af408e27476d8700c5a43f61a1
Sourish Gopietti <sourish.gopisetti@gmail.com>
2026-07-25 13:27:20 -0400

    Add loops.py: demonstrate for and while loops

---
+++ loops.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry"]
+vegetables = ["tomato", "potato", "onion"]
+for x in fruits:
+    print(x)
+
+for y in vegetables:
+    if y == "potato":
+        print("I don't like potatoes")
+        break
+
